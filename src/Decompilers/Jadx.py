899a64327160dc0f0e4b193e106efef7ba2efd77
Joao Rocha <jr007silva1306@gmail.com>
2023-04-18 12:01:18 +0100

    Added logs to Jadx!

--- src/Decompilers/Jadx.py
+++ src/Decompilers/Jadx.py
@@ -4,9 +4,13 @@ import os
 from src.Decompilers.Decompiler import Decompiler
 
 class Jadx(Decompiler):
-    def __init__(self, apkPath, outputFolder="/jadx/"):
+    def __init__(self, apkPath, outputFolder="jadx/"):
         super().__init__(apkPath, outputFolder)
 
     def decompile(self):
-        result = subprocess.run(["cmd", "/c", "jadx", self.apkPath, "-d", self.outputFolder, "-e", "--no-imports"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if not os.path.exists(f"{self.outputFolder}logs/"):
+            os.makedirs(f"{self.outputFolder}logs/")
+        stdoutFile = open(f"{self.outputFolder}logs/out.txt", "w+")
+        stderrFile = open(f"{self.outputFolder}logs/err.txt", "w+")
+        result = subprocess.run(["cmd", "/c", "jadx", self.apkPath, "-d", self.outputFolder, "-e", "--no-imports"], stdout=stdoutFile, stderr=stderrFile)
         #print(result.stdout)
